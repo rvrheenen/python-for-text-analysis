cd53cbd12a77b86ee735a93bd783897ba08ab3f8
rvrheenen <rick.van.rheenen@gmail.com>
2019-09-27 16:18:43 +0200

    forgot to save ass3a thing

--- Assignments/count_words.py
+++ Assignments/count_words.py
@@ -3,5 +3,3 @@ from utils_3a import preprocess
 def count(text):
     words = preprocess(text)
     return {word:words.count(word) for word in set(words)}
-
-print(count("this is a (tricky) test, is this?"))
